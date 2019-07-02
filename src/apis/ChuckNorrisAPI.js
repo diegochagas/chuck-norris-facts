0ebce8014072e4a9b155532b1783612750f9147b
Diego Rocha Chagas <drochachagas@gmail.com>
2019-07-02 17:16:49 -0300

    deploy files

--- src/apis/ChuckNorrisAPI.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const instance = axios.create({ baseURL: 'https://api.chucknorris.io/jokes' });
-
-export const categories = async () => await instance.get(`/categories`);
-
-export const randomJoke = async category => await instance.get(`/random?category=${category}`);
--- src/sagas/index.js
+++
@@ -1,7 +0,0 @@
-import { all } from 'redux-saga/effects';
-import watchCategoriesLoad from './categoriesSaga';
-import watchJokeLoad from './randomJokeSaga';
-
-export default function* rootSaga() {
-    yield all([watchCategoriesLoad(), watchJokeLoad()]);
-}
