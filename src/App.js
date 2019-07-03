75ccc60be04ba555be431f6271a8eb797ba99450
Diego Rocha Chagas <drochachagas@gmail.com>
2019-07-03 17:13:57 -0300

    Replace BrowserRouter for HasRouter to able the page to be refresh

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.scss';
-import { BrowserRouter, Route } from 'react-router-dom'; 
+import { HashRouter, Route } from 'react-router-dom'; 
 import { Provider } from 'react-redux';
 import Header from './components/Header';
 import Categories from './components/Categories';
@@ -13,13 +13,13 @@ function App() {
     return (
         <Provider store={store}>
             <div className="App">
-                <BrowserRouter basename={process.env.PUBLIC_URL}>
+                <HashRouter basename="/">
                     <Header/>
                     <div className="App-body" data-test="app">
                         <Route path="/" exact component={Categories} />
                         <Route path="/jokes/:category" exact component={Jokes} />
                     </div>
-                </BrowserRouter>
+                </HashRouter>
             </div>
         </Provider>
     );
